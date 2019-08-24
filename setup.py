845e9fd8c9820e188f03941c74955323898ea9dd
kevin-meng <meng_fr595@163.com>
2019-08-24 16:21:33 +0800

    init all files

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="python-louvain",
+    version="0.13",
+    author="Thomas Aynaud",
+    author_email="taynaud@gmail.com",
+    description="Louvain algorithm for community detection",
+    license="BSD",
+    url="https://github.com/taynaud/python-louvain",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+    ],
+
+    packages=['community'],
+    install_requires=[
+        "networkx",
+    ],
+
+    scripts=['bin/community']
+)
